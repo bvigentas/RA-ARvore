4e5a42be924d84a0cbc9693a9276a1b9a446b676
bruno.vigentas <brunovigentas@gmail.com>
2021-03-23 20:17:52 -0300

    Initial commit

---
+++ RA-ARVORE/Assets/Scripts/ARCamera.cs
@@ -0,0 +1,352 @@
+using System;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System.Collections;
+using UnityEngine.XR.ARFoundation.Samples;
+
+public class ARCamera : MonoBehaviour
+{
+    private IList<Detector.BoundingBox> boxOutlines;
+    public List<Detector.BoundingBox> boxSavedOutlines = new List<Detector.BoundingBox>();
+    private int staticNum = 0;
+    private bool isDetecting = false;
+    public bool localization = false;
+    public Detector detector;
+    public float shiftY = 0f;
+    public float shiftX = 0f;
+    public float scaleFactor = 1;
+    public Color colorTag = new Color(0.3843137f, 0, 0.9333333f);
+    private static Texture2D boxOutlineTexture;
+    private static GUIStyle labelStyle;
+
+    Texture2D m_CameraTexture;
+
+    [SerializeField]
+    ARCameraManager m_CameraManager;
+
+    public ARCameraManager cameraManager
+    {
+        get => m_CameraManager;
+        set => m_CameraManager = value;
+    }
+
+    [SerializeField]
+    RawImage m_RawImage;
+
+    public RawImage rawImage
+    {
+        get { return m_RawImage; }
+        set { m_RawImage = value; }
+    }
+
+    void OnDisable()
+    {
+        if (m_CameraManager != null)
+        {
+            m_CameraManager.frameReceived -= OnCameraFrameReceived;
+        }
+    }
+
+    public void OnRefresh()
+    {
+        Debug.Log("DEBUG: onRefresh, removing anchors and boundingboxes");
+        localization = false;
+        staticNum = 0;
+        // clear boubding box containers
+        boxSavedOutlines.Clear();
+        boxOutlines.Clear();
+        // clear anchor
+        AnchorCreator anchorCreator = FindObjectOfType<AnchorCreator>();
+        anchorCreator.RemoveAllAnchors();
+    }
+
+    void OnEnable()
+    {
+        if (m_CameraManager != null)
+        {
+            m_CameraManager.frameReceived += OnCameraFrameReceived;
+        }
+
+        boxOutlineTexture = new Texture2D(1, 1);
+        boxOutlineTexture.SetPixel(0, 0, this.colorTag);
+        boxOutlineTexture.Apply();
+
+        labelStyle = new GUIStyle();
+        labelStyle.fontSize = 50;
+        labelStyle.normal.textColor = this.colorTag;
+
+        detector = GameObject.Find("Detector").GetComponent<Detector>();
+
+        detector.Start();
+
+        CalculateShift(this.detector.IMAGE_SIZE);
+    }
+
+    private void CalculateShift(int inputSize)
+    {
+        int smallest;
+
+        if (Screen.width < Screen.height)
+        {
+            smallest = Screen.width;
+            this.shiftY = (Screen.height - smallest) / 2f;
+        }
+        else
+        {
+            smallest = Screen.height;
+            this.shiftX = (Screen.width - smallest) / 2f;
+        }
+
+        this.scaleFactor = smallest / (float)inputSize;
+    }
+
+    unsafe void OnCameraFrameReceived(ARCameraFrameEventArgs eventArgs)
+    {
+        XRCpuImage image;
+        if (!cameraManager.TryAcquireLatestCpuImage(out image))
+        {
+            return;
+        }
+
+        var format = TextureFormat.RGBA32;
+
+        if (m_CameraTexture == null || m_CameraTexture.width != image.width || m_CameraTexture.height != image.height)
+        {
+            m_CameraTexture = new Texture2D(image.width, image.height, format, false);
+        }
+
+        var conversionParams = new XRCpuImage.ConversionParams(image, format, XRCpuImage.Transformation.None);
+        var rawTextureData = m_CameraTexture.GetRawTextureData<byte>();
+        try
+        {
+            image.Convert(conversionParams, new IntPtr(rawTextureData.GetUnsafePtr()), rawTextureData.Length);
+        }
+        finally
+        {
+            image.Dispose();
+        }
+
+        m_CameraTexture.Apply();
+
+        if (staticNum > 10)
+        {
+            localization = true;
+        }
+        else
+        {
+            if (this.isDetecting)
+            {
+                return;
+            }
+
+            this.isDetecting = true;
+            StartCoroutine(ProcessImage(this.detector.IMAGE_SIZE, result =>
+            {
+                StartCoroutine(this.detector.Detect(result, boxes =>
+                {
+                    this.boxOutlines = boxes;
+                    Resources.UnloadUnusedAssets();
+                    this.isDetecting = false;
+                }));
+            }));
+
+            GroupBoxOutlines();
+        }
+        m_RawImage.texture = m_CameraTexture;
+    }
+
+    private IEnumerator ProcessImage(int inputSize, System.Action<Color32[]> callback)
+    {
+        Coroutine croped = StartCoroutine(TextureTools.CropSquare(m_CameraTexture,
+           TextureTools.RectOptions.Center, snap =>
+           {
+               var scaled = Scale(snap, inputSize);
+               var rotated = Rotate(scaled.GetPixels32(), scaled.width, scaled.height);
+               callback(rotated);
+           }));
+        yield return croped;
+    }
+
+    private void GroupBoxOutlines()
+    {
+        // if savedoutlines is empty, add current frame outlines if possible.
+        if (this.boxSavedOutlines.Count == 0)
+        {
+            // no bounding boxes in current frame
+            if (this.boxOutlines == null || this.boxOutlines.Count == 0)
+            {
+                return;
+            }
+            // deep copy current frame bounding boxes
+            foreach (var outline in this.boxOutlines)
+            {
+                this.boxSavedOutlines.Add(outline);
+            }
+            return;
+        }
+
+        // adding current frame outlines to existing savedOulines and merge if possible.
+        bool addOutline = false;
+        foreach (var outline1 in this.boxOutlines)
+        {
+            bool unique = true;
+            foreach (var outline2 in this.boxSavedOutlines)
+            {
+                // if two bounding boxes are for the same object, use high confidnece one
+                if (IsSameObject(outline1, outline2))
+                {
+                    unique = false;
+                    if (outline1.Confidence > outline2.Confidence + 0.05F) //& outline2.Confidence < 0.5F)
+                    {
+                        Debug.Log("DEBUG: add detected boxes in this frame.");
+                        Debug.Log($"DEBUG: Add Label: {outline1.Label}. Confidence: {outline1.Confidence}.");
+                        Debug.Log($"DEBUG: Remove Label: {outline2.Label}. Confidence: {outline2.Confidence}.");
+
+                        this.boxSavedOutlines.Remove(outline2);
+                        this.boxSavedOutlines.Add(outline1);
+                        addOutline = true;
+                        staticNum = 0;
+                        break;
+                    }
+                }
+            }
+            // if outline1 in current frame is unique, add it permanently
+            if (unique)
+            {
+                Debug.Log($"DEBUG: add detected boxes in this frame");
+                addOutline = true;
+                staticNum = 0;
+                this.boxSavedOutlines.Add(outline1);
+                Debug.Log($"Add Label: {outline1.Label}. Confidence: {outline1.Confidence}.");
+            }
+        }
+        if (!addOutline)
+        {
+            staticNum += 1;
+        }
+
+        // merge same bounding boxes
+        // remove will cause duplicated bounding box?
+        List<Detector.BoundingBox> temp = new List<Detector.BoundingBox>();
+        foreach (var outline1 in this.boxSavedOutlines)
+        {
+            if (temp.Count == 0)
+            {
+                temp.Add(outline1);
+                continue;
+            }
+            foreach (var outline2 in temp.ToList())
+            {
+                if (IsSameObject(outline1, outline2))
+                {
+                    if (outline1.Confidence > outline2.Confidence)
+                    {
+                        temp.Remove(outline2);
+                        temp.Add(outline1);
+                        Debug.Log("DEBUG: merge bounding box conflict!!!");
+                    }
+                }
+                else
+                {
+                    temp.Add(outline1);
+                }
+            }
+        }
+        this.boxSavedOutlines = temp;
+    }
+
+    private bool IsSameObject(Detector.BoundingBox outline1, Detector.BoundingBox outline2)
+    {
+        var xMin1 = outline1.Dimensions.X * this.scaleFactor + this.shiftX;
+        var width1 = outline1.Dimensions.Width * this.scaleFactor;
+        var yMin1 = outline1.Dimensions.Y * this.scaleFactor + this.shiftY;
+        var height1 = outline1.Dimensions.Height * this.scaleFactor;
+        float center_x1 = xMin1 + width1 / 2f;
+        float center_y1 = yMin1 + height1 / 2f;
+
+        var xMin2 = outline2.Dimensions.X * this.scaleFactor + this.shiftX;
+        var width2 = outline2.Dimensions.Width * this.scaleFactor;
+        var yMin2 = outline2.Dimensions.Y * this.scaleFactor + this.shiftY;
+        var height2 = outline2.Dimensions.Height * this.scaleFactor;
+        float center_x2 = xMin2 + width2 / 2f;
+        float center_y2 = yMin2 + height2 / 2f;
+
+        bool cover_x = (xMin2 < center_x1) && (center_x1 < (xMin2 + width2));
+        bool cover_y = (yMin2 < center_y1) && (center_y1 < (yMin2 + height2));
+        bool contain_x = (xMin1 < center_x2) && (center_x2 < (xMin1 + width1));
+        bool contain_y = (yMin1 < center_y2) && (center_y2 < (yMin1 + height1));
+
+        return (cover_x && cover_y) || (contain_x && contain_y);
+    }
+
+    public void OnGUI()
+    {
+        // Do not draw bounding boxes after localization.
+        if (localization)
+        {
+            return;
+        }
+
+        if (this.boxSavedOutlines != null && this.boxSavedOutlines.Any())
+        {
+            foreach (var outline in this.boxSavedOutlines)
+            {
+                DrawBoxOutline(outline, scaleFactor, shiftX, shiftY);
+            }
+        }
+    }
+
+    private void DrawBoxOutline(Detector.BoundingBox outline, float scaleFactor, float shiftX, float shiftY)
+    {
+        var x = outline.Dimensions.X * scaleFactor + shiftX;
+        var width = outline.Dimensions.Width * scaleFactor;
+        var y = outline.Dimensions.Y * scaleFactor + shiftY;
+        var height = outline.Dimensions.Height * scaleFactor;
+
+        DrawRectangle(new Rect(x, y, width, height), 10, this.colorTag);
+        DrawLabel(new Rect(x, y - 80, 200, 20), $"Localizing {outline.Label}: {(int)(outline.Confidence * 100)}%");
+    }
+
+    public static void DrawRectangle(Rect area, int frameWidth, Color color)
+    {
+        Rect lineArea = area;
+        lineArea.height = frameWidth;
+        GUI.DrawTexture(lineArea, boxOutlineTexture); // Top line
+
+        lineArea.y = area.yMax - frameWidth;
+        GUI.DrawTexture(lineArea, boxOutlineTexture); // Bottom line
+
+        lineArea = area;
+        lineArea.width = frameWidth;
+        GUI.DrawTexture(lineArea, boxOutlineTexture); // Left line
+
+        lineArea.x = area.xMax - frameWidth;
+        GUI.DrawTexture(lineArea, boxOutlineTexture); // Right line
+    }
+
+
+    private static void DrawLabel(Rect position, string text)
+    {
+        GUI.Label(position, text, labelStyle);
+    }
+
+    private Texture2D Scale(Texture2D texture, int imageSize)
+    {
+        var scaled = TextureTools.scaled(texture, imageSize, imageSize, FilterMode.Bilinear);
+        return scaled;
+    }
+
+    private Color32[] Rotate(Color32[] pixels, int width, int height)
+    {
+        var rotate = TextureTools.RotateImageMatrix(
+                pixels, width, height, 90);
+        return rotate;
+    }
+
+
+}
---
+++ RA-ARVORE/Assets/Scripts/AnchorCreator.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.ARFoundation;
+
+
+[RequireComponent(typeof(ARAnchorManager))]
+[RequireComponent(typeof(ARRaycastManager))]
+public class AnchorCreator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_Prefab;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    IDictionary<ARAnchor, Detector.BoundingBox> anchorDic = new Dictionary<ARAnchor, Detector.BoundingBox>();
+    private List<Detector.BoundingBox> boxSavedOutlines;
+    private float shiftX;
+    private float shiftY;
+    private float scaleFactor;
+
+    public ARCamera aRCamera;
+    public ARRaycastManager m_RaycastManager;
+    public TextMesh anchorObj_mesh;
+    public ARAnchorManager m_AnchorManager;
+
+    public GameObject prefab
+    {
+        get => m_Prefab;
+        set => m_Prefab = value;
+    }
+
+    public void RemoveAllAnchors()
+    {
+        Debug.Log($"DEBUG: Removing all anchors ({anchorDic.Count})");
+        foreach (var anchor in anchorDic)
+        {
+            Destroy(anchor.Key.gameObject);
+        }
+        s_Hits.Clear();
+        anchorDic.Clear();
+    }
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+        m_AnchorManager = GetComponent<ARAnchorManager>();
+        GameObject cameraImage = GameObject.Find("Camera");
+        aRCamera = cameraImage.GetComponent<ARCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update in action");
+        if (!aRCamera.localization)
+        {
+            return;
+        }
+
+        boxSavedOutlines = aRCamera.boxSavedOutlines;
+        shiftX = aRCamera.shiftX;
+        shiftY = aRCamera.shiftY;
+        scaleFactor = aRCamera.scaleFactor;
+
+        if (anchorDic.Count != 0)
+        {
+            foreach (KeyValuePair<ARAnchor, Detector.BoundingBox> pair in anchorDic)
+            {
+                if (!boxSavedOutlines.Contains(pair.Value))
+                {
+                    anchorDic.Remove(pair.Key);
+                    m_AnchorManager.RemoveAnchor(pair.Key);
+                    s_Hits.Clear();
+                }
+            }
+        }
+
+        if (boxSavedOutlines.Count == 0)
+        {
+            return;
+        }
+
+        foreach (var outline in boxSavedOutlines)
+        {
+            if (outline.Used)
+            {
+                continue;
+            }
+
+            var xMin = outline.Dimensions.X * this.scaleFactor + this.shiftX;
+            var width = outline.Dimensions.Width * this.scaleFactor;
+            var yMin = outline.Dimensions.Y * this.scaleFactor + this.shiftY;
+            yMin = Screen.height - yMin;
+            var height = outline.Dimensions.Height * this.scaleFactor;
+
+            float center_x = xMin + width / 2f;
+            float center_y = yMin - height / 2f;
+
+            if (Pos2Anchor(center_x, center_y, outline))
+            {
+                outline.Used = true;
+
+
+            }
+        }
+    }
+
+    private bool Pos2Anchor(float x, float y, Detector.BoundingBox outline)
+    {
+        const TrackableType trackableTypes =
+            TrackableType.FeaturePoint |
+            TrackableType.PlaneWithinPolygon;
+
+        if (m_RaycastManager.Raycast(new Vector2(x, y), s_Hits, trackableTypes))
+        {
+            var hit = s_Hits[0];
+            var anchor = CreateAnchor(hit);
+            if (anchor)
+            {
+                // Remember the anchor so we can remove it later.
+                anchorDic.Add(anchor, outline);
+                return true;
+            }
+
+            return false;
+        }
+        return false;
+    }
+
+    ARAnchor CreateAnchor(in ARRaycastHit hit)
+    {
+        ARAnchor anchor = null;
+
+        if (hit.trackable is ARPlane plane)
+        {
+            var planeManager = GetComponent<ARPlaneManager>();
+            if (planeManager)
+            {
+                var oldPrefab = m_AnchorManager.anchorPrefab;
+                m_AnchorManager.anchorPrefab = prefab;
+                anchor = m_AnchorManager.AttachAnchor(plane, hit.pose);
+                m_AnchorManager.anchorPrefab = oldPrefab;
+                return anchor;
+            }
+        }
+        var gameObject = Instantiate(prefab, hit.pose.position, hit.pose.rotation);
+
+        anchor = gameObject.GetComponent<ARAnchor>();
+        if (anchor == null)
+        {
+            anchor = gameObject.AddComponent<ARAnchor>();
+        }
+
+        return anchor;
+    }
+}
