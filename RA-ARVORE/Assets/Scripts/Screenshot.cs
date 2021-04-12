be8ea4dfeb1043d57e95bd5ef2f839d3f3ceef61
bruno.vigentas <brunovigentas@gmail.com>
2021-04-12 04:27:14 -0300

    Button

--- RA-ARVORE/Assets/Scripts/Screenshot.cs
+++ RA-ARVORE/Assets/Scripts/Screenshot.cs
@@ -4,21 +4,9 @@ using UnityEngine;
 
 public class Screenshot : MonoBehaviour
 {
-
-    [SerializeField]
-    GameObject blink;
     public void buttonScreenShot()
     {
-        StartCoroutine("CaptureScreen");
-    }
-
-    IEnumerator CaptureScreen()
-    {
         var timestamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
-        string fileName = "ARvore" + timestamp + ".png";
-        string savePath = fileName;
-        ScreenCapture.CaptureScreenshot(savePath);
-        yield return new WaitForEndOfFrame();
-        Instantiate(blink, new Vector2(0f, 0f), Quaternion.identity);
+        string fileName = "ARvore-Screenshot-" + timestamp;
     }
 }
