e0473db025c93e463727946796ff5c3dcef0c3fa
bruno.vigentas <brunovigentas@gmail.com>
2021-04-13 02:46:20 -0300

    screenshot e multiloabada

--- RA-ARVORE/Assets/Scripts/Screenshot.cs
+++ RA-ARVORE/Assets/Scripts/Screenshot.cs
@@ -8,5 +8,20 @@ public class Screenshot : MonoBehaviour
     {
         var timestamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
         string fileName = "ARvore-Screenshot-" + timestamp;
+        StartCoroutine(TakeScreenshot());
+    }
+
+    private IEnumerator TakeScreenshot()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
+
+        var timestamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+        string fileName = "ARvore-Screenshot-" + timestamp;
+
+        NativeGallery.SaveImageToGallery(ss, "ARvore Screenshots", fileName);
     }
 }
